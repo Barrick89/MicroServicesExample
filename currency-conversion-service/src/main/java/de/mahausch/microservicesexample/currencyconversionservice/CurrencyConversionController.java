013bea22b5bb7022c1541da94da95eaee930611f
Markus Hauschild <mhauschild@united-internet.de>
2018-10-24 13:59:30 +0200

    Add currency-exchange-service and currency-calculation-service

---
+++ currency-conversion-service/src/main/java/de/mahausch/microservicesexample/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,8 @@
+package de.mahausch.microservicesexample.currencyconversionservice;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyConversionController {
+
+}
