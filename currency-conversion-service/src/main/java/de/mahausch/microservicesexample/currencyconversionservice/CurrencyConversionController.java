8583209ebf87e503ca9cd1fd9dabbe94546da4cc
Markus Hauschild <mhauschild@united-internet.de>
2018-12-07 09:59:26 +0100

    Add feign and the CurrencyConversionBean to make requests to the CurrencyExchangeService more easily

--- currency-conversion-service/src/main/java/de/mahausch/microservicesexample/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/de/mahausch/microservicesexample/currencyconversionservice/CurrencyConversionController.java
@@ -1,8 +1,27 @@
 package de.mahausch.microservicesexample.currencyconversionservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 @RestController
 public class CurrencyConversionController {
 
+    @Autowired
+    private CurrencyExchangeServiceProxy proxy;
+
+    @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrency(@PathVariable String from,
+                                                    @PathVariable String to,
+                                                    @PathVariable BigDecimal quantity){
+
+        CurrencyConversionBean response = proxy.retrieveExchangeValue(from,to);
+
+        return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(),
+                quantity, quantity.multiply(response.getConversionMultiple()));
+
+    }
 }
--- currency-conversion-service/src/main/java/de/mahausch/microservicesexample/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/de/mahausch/microservicesexample/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package de.mahausch.microservicesexample.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("de.mahausch.microservicesexample.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
